c679db62d138959eff647f71dd0bb9bad9cdbb43
Miroslav Hadzhiev <miroslav.hadzhiev@gmail.com>
2020-03-10 22:28:04 +0000

    List EC2 Instances. List S3 Buckets.

---
+++ list-ec2-to-txt-s3.py
@@ -0,0 +1,33 @@
+import json
+import boto3
+
+ec2 = boto3.client('ec2')
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+
+    filters = [
+        {
+            'Name': 'instance-state-name',
+            'Values': ['*']
+        }
+    ]
+
+    instances = ec2.instances.filter(Filters=filters)
+
+    RunningInstances = []
+
+    for instance in instances:
+        RunningInstances.append(instance.id)
+
+        instanceList = json.dumps(RunningInstances)
+
+        s3.Object(
+            'kodyaz-com-aws',
+            'instanceList.txt').put(Body=instanceList)
+
+    return {
+        'statusCode': 200,
+        'body': instanceList
+    }
---
+++ list-s3-buckets.py
@@ -0,0 +1,17 @@
+import json
+import boto3
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+
+    bucketlist = []
+
+    for bucket in s3.list_buckets():
+        bucketlist.append(bucket.name)
+
+    return {
+        "statusCode": 200,
+        "body": bucketlist
+    }
